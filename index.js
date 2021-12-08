f5409526c9fc48ec450b23e447f56c300453eca0
comstudy21@naver.com <comstudy21@naver.com>
2021-12-08 11:11:23 +0900

    commit step2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("nodejs server test");
+const express = require("express");
+const http = require("http");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("nodejs server test");
+  res.end("<h1>Hello world</h1>");
+});
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+  console.log("서버 실행 중 >>> http://localhost:3000");
+});
