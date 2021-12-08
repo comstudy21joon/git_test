dc6a1fb673aa710459aeb4b1af90bb30358844ba
comstudy21@naver.com <comstudy21@naver.com>
2021-12-08 11:35:45 +0900

    commit step 03_2

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ app.get("/", (req, res) => {
   res.end("<h1>Hello world</h1>");
 });
 
+app.get("/car/list", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+  res.write("<h2>자동차 목록</h2>");
+  res.end();
+});
+
 const server = http.createServer(app);
 server.listen(3000, () => {
   console.log("서버 실행 중 >>> http://localhost:3000");
